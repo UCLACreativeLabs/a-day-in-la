db1a2bf8193adaa16b7ccc84c91d3bdcbe1006c1
Chang Liu <shampliu@gmail.com>
2017-02-17 20:27:25 -0800

    add svg asset

--- scripts/santamonica.js
+++ scripts/santamonica.js
@@ -1,11 +1,25 @@
 var s = Snap("#santamonica");
 
-var santamonica = Snap.load("assets/samo.svg", function ( loadedFragment ) {
+var santamonica = Snap.load("assets/scene1.svg", function ( loadedFragment ) {
   s.append( loadedFragment );
 
-  var mask = s.rect(400,150,600,400).attr({ stroke: 'silver', 'strokeWidth': 40, fill: 'silver', id: 'mask' });
+  // var mask = s.rect(400,150,600,400).attr({ stroke: 'silver', 'strokeWidth': 40, fill: 'silver', id: 'mask' });
   // var g = s.group(loadedFragment);
-  s.attr({ mask: mask });
+  // s.attr({ mask: mask });
+
+  // animate{
+  //   waves: {
+  //     trigger: "default", "click", "hover"
+  //     animation: surferAnimation()
+  //     easein:
+  //     easeout:
+  //     hotspot: x: , y: // or target
+  //   }
+  // }
+
+  // function surferAnimation() {
+  //
+  // }
   //
   // s.append(g);
   // g.hover( hoverover, hoverout );
