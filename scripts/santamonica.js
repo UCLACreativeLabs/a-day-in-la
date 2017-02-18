2ceeb82d5e9cc4b58ac72ee6e59b5d3cabc49de5
Chang Liu <shampliu@gmail.com>
2017-02-17 21:16:19 -0800

    ghetto panning on mouse pos

--- scripts/santamonica.js
+++ scripts/santamonica.js
@@ -1,10 +1,35 @@
 var s = Snap("#santamonica");
 
+var w = window.innerWidth;
+var h = window.innerHeight;
+var mid = { x: Math.round(w/2), y: Math.round(h/2) };
+var pad = 200;
+
+
 var santamonica = Snap.load("assets/scene1.svg", function ( loadedFragment ) {
   s.append( loadedFragment );
 
+  var svg = document.getElementById('santamonica');
+
+  window.addEventListener('mousemove', function(e) {
+    if (e.pageX < (mid.x - pad)) {
+      let currX = 0;
+      (function(currX) {
+        setInterval(
+          function() {
+            currX -= 10;
+            svg.style.transform = "translateX(" + currX + "px)";
+
+          }, 200
+        )
+
+      })(currX);
+
+    }
+
+  });
+
   // var mask = s.rect(400,150,600,400).attr({ stroke: 'silver', 'strokeWidth': 40, fill: 'silver', id: 'mask' });
-  // var g = s.group(loadedFragment);
   // s.attr({ mask: mask });
 
   // animate{
