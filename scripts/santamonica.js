44f0a75f8c7432055eb9cb74b1706165e03d0cf6
Chang Liu <shampliu@gmail.com>
2017-02-19 00:48:17 -0800

    panning works but padding is glitchy

--- scripts/santamonica.js
+++ scripts/santamonica.js
@@ -3,29 +3,42 @@ var s = Snap("#santamonica");
 var w = window.innerWidth;
 var h = window.innerHeight;
 var mid = { x: Math.round(w/2), y: Math.round(h/2) };
-var pad = 200;
+var pad = 1;
+var scale = 1.2;
 
 
 var santamonica = Snap.load("assets/scene1.svg", function ( loadedFragment ) {
   s.append( loadedFragment );
 
   var svg = document.getElementById('santamonica');
+  svg.style.width = w + 'px';
+  svg.style.height = h + 'px';
+  svg.style.transformOrigin = '50% 50%';
 
   window.addEventListener('mousemove', function(e) {
-    if (e.pageX < (mid.x - pad)) {
-      let currX = 0;
-      (function(currX) {
-        setInterval(
-          function() {
-            currX -= 10;
-            svg.style.transform = "translateX(" + currX + "px)";
+    let dx = e.pageX - mid.x;
+    let dy = e.pageY - mid.y;
 
-          }, 200
-        )
+    let newX = -1 * Math.min((w / 10) * (dx / mid.x) * pad, w/10);
+    let newY = -1 * Math.min((h / 10) * (dy / mid.y) * pad, h/10);
 
-      })(currX);
+    // svg.style.transform = "translate(" + percX + "%," + percY + "%)";
+    svg.style.transform = `matrix(${scale}, 0, 0, ${scale}, ${newX}, ${newY})`
 
-    }
+
+
+
+    // if (e.pageX < (mid.x - pad)) {
+    //   console.log('translating');
+    //   let currX = 0;
+    //   currX = -100;
+    //   let currY = -100;
+    //   (function(currX) {
+    //     svg.style.transform = "translate(" + currX + "px," + currY + "px)";
+    //
+    //   })(currX, currY);
+    //
+    // }
 
   });
 
