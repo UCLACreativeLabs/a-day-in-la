4fc2b01b81bb5a7bfedcac3b35876ea93641f34f
Yunong Jiang <yunong@MacBook-Pro.local>
2017-03-03 19:25:19 -0800

    added cursor feature

--- scripts/santamonica.js
+++ scripts/santamonica.js
@@ -25,3 +25,49 @@ var santamonica = Snap.load("assets/scene1.svg", function ( loadedFragment ) {
   // g.hover( hoverover, hoverout );
   // g.text(300,100, 'hover over me');
 });
+
+var cursor = s.circle(150,150,8);
+s.addClass("cursor");
+cursor.attr({
+    fill: "#ddd",
+    stroke: "#ddd",
+    strokeWidth: 2,
+    opacity: 0.8
+});
+
+function animateExpand() {
+    cursor.animate({r: 13},
+        800,
+        animateShrink
+    );
+}
+
+function animateShrink() {
+    cursor.animate({r: 8},
+        800,
+        animateExpand
+    )
+}
+
+animateExpand();
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("santamonica").onmousemove = function(evt) {
+      //console.log(evt.clientX + ", " + evt.clientY);
+      cursor.attr({
+          cx: evt.clientX,
+          cy: evt.clientY
+      });
+  };
+    document.getElementById("santamonica").onmousedown = function() {
+      cursor.animate({
+        opacity: 0,
+          r: 20
+      }, 800, function() {
+        cursor.attr({
+          opacity: 0.8,
+            r: 10
+        })
+      });
+    };
+});
