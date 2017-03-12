e5cb55d473e3c74cab3c8bcc4f5daba2c862d54d
Max Woo <maxithewoowoo@gmail.com>
2017-03-12 15:46:54 -0700

    tweaks

--- scripts/animatealongpath.js
+++ scripts/animatealongpath.js
@@ -7,9 +7,9 @@ function transformBetween(src, dest) {
 }
 
 // given a list of milestones, and assuming it takes the same amount of time to
-// get to the next milestone, converts a value between [0,1] to the corresponding path
+// get to the next milestone, converts a value between [0,1] to the corresponding travel
 // length it should reach
-function makeTravelInterpolator(milestones) {
+function createTravelLengthInterpolator(milestones) {
 	var index = 0,
 		prevLength = 0,
 		interval = milestones[index] - prevLength; // distance between prev and next milestone
@@ -22,14 +22,17 @@ function makeTravelInterpolator(milestones) {
 			interval = milestones[index] - prevLength;
 		}
 		var progress = value*milestones.length - index; // how far towards our next milestone
-		return prevLength + progress*interval
+		return prevLength + progress*interval;
 	}
 }
 
 // modified from http://michaeltempest.com/the-missing-snap-svg-function/
-// "milestones" are control points to control speed along the path. The time between each milestone is constant, so put milestones close together on sections you want the object to move slowly
+// "milestones" are control points to control speed along the path. The time between each milestone
+// is constant, so put milestones close together on sections you want the object to move slowly
+// 
 // milestones: array of points along the path (in absolute distance)
-// duration: the time it should take to get to the next milestone
+// duration: the TOTAL duraction of the animation. Thus, each milestone
+//           takes (duration/milestones.length) amount of time
 function animateAlongPath(path, el, start, duration, milestones, animObj, easing, callback) {
 	var len = Snap.path.getTotalLength(path),
 		elBB = el.getBBox(),
@@ -38,7 +41,7 @@ function animateAlongPath(path, el, start, duration, milestones, animObj, easing
 
 	milestones.push(len); // add the path end point as a final milestone
 
-	var getLength = makeTravelInterpolator(milestones);
+	var getLength = createTravelLengthInterpolator(milestones);
 
 	animObj.anim = Snap.animate(0, 1, function(value) {
 		var pt = path.getPointAtLength(getLength(value));
