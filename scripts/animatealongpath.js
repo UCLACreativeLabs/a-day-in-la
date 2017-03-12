d904283ecfa302a01f8fe38f832a63beb30d2d8c
Max Woo <maxithewoowoo@gmail.com>
2017-03-12 04:12:32 -0700

    makeTravelInterpolator()
    interpolates between [0,1] to a total path length traveled

--- scripts/animatealongpath.js
+++ scripts/animatealongpath.js
@@ -6,6 +6,26 @@ function transformBetween(src, dest) {
 	return srcTransform.multLeft(destTransform.invert());
 }
 
+// given a list of milestones, and assuming it takes the same amount of time to
+// get to the next milestone, converts a value between [0,1] to the corresponding path
+// length it should reach
+function makeTravelInterpolator(milestones) {
+	var index = 0,
+		prevLength = 0,
+		interval = milestones[index] - prevLength; // distance between prev and next milestone
+
+	return (value) => {
+		if (value >= 1) return milestones[milestones.length-1]; // last milestone
+		else if (value > (index+1)/milestones.length) {
+			index++;
+			prevLength = milestones[index-1];
+			interval = milestones[index] - prevLength;
+		}
+		var progress = value*milestones.length - index; // how far towards our next milestone
+		return prevLength + progress*interval
+	}
+}
+
 // modified from http://michaeltempest.com/the-missing-snap-svg-function/
 // "milestones" are control points to control speed along the path. The time between each milestone is constant, so put milestones close together on sections you want the object to move slowly
 // milestones: array of points along the path (in absolute distance)
@@ -18,15 +38,13 @@ function animateAlongPath(path, el, start, duration, milestones, animObj, easing
 
 	milestones.push(len); // add the path end point as a final milestone
 
-	(function startFrame(currFrame, prevEnd) {
-		var nextEnd = milestones[currFrame];
-		animObj.anim = Snap.animate(prevEnd, nextEnd, function(value) {
-			var pt = path.getPointAtLength(value);
-			el.transform('t' + (mat.x(pt.x,pt.y) - elBB.cx) + ',' + (mat.y(pt.x,pt.y) - elBB.cy));
-			//console.log(el.transform().string)
-		}, duration, easing, function() {
-			if (currFrame < milestones.length) startFrame(currFrame+1, nextEnd);
-			else if (callback) callback(path);
-		});
-	})(0, start);
+	var getLength = makeTravelInterpolator(milestones);
+
+	animObj.anim = Snap.animate(0, 1, function(value) {
+		var pt = path.getPointAtLength(getLength(value));
+		el.transform('t' + (mat.x(pt.x,pt.y) - elBB.cx) + ',' + (mat.y(pt.x,pt.y) - elBB.cy));
+		//console.log(el.transform().string)
+	}, duration, easing, function() {
+		if (callback) callback(path);
+	});
 };
