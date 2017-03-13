adbbb5f02c24bfb7981c17abf134760b24e13c51
Max Woo <maxithewoowoo@gmail.com>
2017-03-13 01:53:05 -0700

    separated milestone interactive builder into separate js file

---
+++ scripts/milestonebuilder.js
@@ -0,0 +1,41 @@
+var oldAnimateAlongPathFn = animateAlongPath;
+animateAlongPath = function(path, el, start, duration, milestones, easing, callback) {
+	var anim = null,
+		args = arguments;
+
+	function reanimate() {
+		anim && anim.stop();
+		el.transform(''); // initial transformation should be 0
+		anim = oldAnimateAlongPathFn(...args);
+	}
+
+	$("#svg").click(function (e) {
+		var matinv = path.transform().globalMatrix.invert(),
+			x = e.offsetX,
+			y = e.offsetY;
+
+		closest = Snap.closestPoint(path, matinv.x(x,y), matinv.y(x,y));
+		if (closest.distance < 5) {
+			createMilestone(path, closest.length, milestones, reanimate);
+		}
+	});
+
+	reanimate();
+}
+
+function createMilestone(path, length, milestones, reanimateFn) {
+	var pt = path.getPointAtLength(length),
+		elem = path.parent().circle(pt.x, pt.y, 5);
+
+	milestones.push(length);
+	milestones.sort((a,b) => a-b);
+	elem.insertAfter(path);
+	elem.click((e) => {
+		milestones.splice(milestones.indexOf(length), 1);
+		elem.remove();
+		e.preventDefault();
+		e.stopPropagation();
+		reanimateFn();
+	});
+	reanimateFn();
+}
