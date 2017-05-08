b6b7cc744547f3fc321eedbbe9642fd8e0d5d25d
rishabhaggarwal2 <rishabhaggarwal2@gmail.com>
2017-05-08 02:55:07 -0700

    popups basic style

--- scripts/Tips.js
+++ scripts/Tips.js
@@ -1,3 +1,31 @@
-function renderTips(tips) {
-	
+// function renderTips(tips) {
+// 	tips.forEach( function(key, value, index) {
+// 		var container = document.getElementById("tipsContainer");
+
+
+// 	});
+
+// 	//show tips with timeouts
+
+// }
+
+// function destroyTips(tips) {
+// 	//hide tips with timeouts
+// 	tips.forEach( function(key, value, index) {
+// 		var container = document.getElementById("tipsContainer");
+
+
+// 	});
+// }
+
+
+
+
+function initTips() {
+	var opacity = 0;
+	window.addEventListener('keypress', function(e) {
+		opacity = 1 - opacity;
+		document.getElementsByClassName('tipsContainerParent')[0].style.opacity = opacity;
+
+	})
 }
